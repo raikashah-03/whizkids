a1c49d5438d206809fa9efc053521168649266ba
mani254 <manikantadev254@gmail.com>
2026-04-13 08:45:33 +0530

    feat: initialize Next.js web application with Tailwind CSS and custom font configuration

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { DynaPuff, Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+
+const dynaPuff = DynaPuff({
+  variable: "--font-dynapuff",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -22,10 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={`${nunito.variable} ${dynaPuff.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
